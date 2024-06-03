63fb23000c04f817c14e195a460847fbca34c58f
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-03 02:29:16 -0700

    es6-async

---
+++ app.js
@@ -0,0 +1,11 @@
+// function hello(){
+//     const num = 80;
+//     if(num > 70){
+//         console.log('behtreen');
+//     }else if(num > 50){
+//         console.log('pass');
+//     }else{
+//         console.log('not pass');
+//     }
+// }
+// hello()
