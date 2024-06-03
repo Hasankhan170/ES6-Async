0482a0c090e565a2088342b8bcf7b6283b5541a8
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-03 02:43:48 -0700

    es6-async

--- app.js
+++ app.js
@@ -9,3 +9,58 @@
 //     }
 // }
 // hello()
+
+
+// async await 
+
+// async function hello(){
+//     const res = await axios("https://fakestoreapi.com/products")
+//     console.log(res.data);
+// }
+
+// hello()
+
+
+
+
+
+// Variable Scoping
+// Closure
+// Generators
+// Modules
+// Exponentiation Operator
+// Optional chaining
+// Object Methods (keys,values,freez,entries)
+// Destructuring //p
+// Template literals //p
+// Destructuring //p
+// Default parameters //p
+// Rest parameter //p
+// Spread Operator //p
+// Arrow functions  //p
+// Enhanced object literals //p
+// Iterators & For..of  //p
+// Map  //p
+// Array methods  //p
+// Higher-order function  //p
+// CallBack  //p
+// Promises  //p
+// Ternary Operator //p
+
+
+
+// Destructuring // ek obj ko break krne ka kam krta ha
+
+// const person = {
+//     userName: 'Mohammad',
+//     age: 20,
+//     address: {
+//         city: 'Karachi',
+//         country: 'Pakistan'
+//     }
+// }
+
+// const {userName,age,address:{city,country}} = person;
+
+// console.log(userName,age,{country});
+
